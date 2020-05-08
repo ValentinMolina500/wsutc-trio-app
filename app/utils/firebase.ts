c4648da1d4b323c52de6d2c434439201d26de048
Valentin Mollina <ValentinMolina500>
2020-05-08 15:19:18 -0700

    Fix repeated message bug

--- app/utils/firebase.ts
+++ app/utils/firebase.ts
@@ -22,6 +22,7 @@ export class Firebase {
             }
         })
             .then(async () => {
+                console.log("******* <3 :) *******")
                 messagesSubject.setMessages();
                 messagesSubject.register(DMViewModel);
             })
